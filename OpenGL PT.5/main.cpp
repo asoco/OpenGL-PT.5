4db52d3308a2c30d75b30cbad31eff94e735092c
Даниил Потемка <daniil@imac-daniil.local>
2019-05-19 12:46:59 +0300

    Initial Commit

---
+++ OpenGL PT.5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL PT.5
+//
+//  Created by Даниил on 19/05/2019.
+//  Copyright © 2019 aoaoaommm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
